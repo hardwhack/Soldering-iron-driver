576ce76e8aab3d4bb9ca418fa26d141805ac50b6
Peter Kim <pjkim00@gmail.com>
2013-05-23 15:10:58 -0700

    build dir removed

--- build/no_bootloader/production/pid.o.d
+++
@@ -1,7 +0,0 @@
-build/no_bootloader/production/pid.o : \
-	/Users/pjkim/MPLABXProjects/arhimed-sid/SID/SID_GEN4.X/pid.c \
-	/Applications/microchip/mplabc18/v3.40/h/delays.h \
-	/Users/pjkim/MPLABXProjects/arhimed-sid/SID/SID_GEN4.X/hardware.h \
-	/Applications/microchip/mplabc18/v3.40/h/p18cxxx.h \
-	/Applications/microchip/mplabc18/v3.40/h/p18f2550.h \
-
